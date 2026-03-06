ae2233ccb2cd83edc5e0b199ac5a9b68f37463ec
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 13:40:34 +0300

    Create ComputerCase

--- creational/src/factory/StorageFactory.java
+++ creational/src/factory/StorageFactory.java
@@ -1,4 +1,12 @@
 package factory;
-
 public class StorageFactory {
+    public static StorageDrive createStorage(String type) {
+        switch (type.toLowerCase()) {
+            case "hdd": return new HddDrive();
+            case "ssd": return new SataSsdDrive();
+            case "nvme": return new NvmeDrive();
+            case "external": return new ExternalUsbDrive();
+            default: throw new IllegalArgumentException("Unknown storage type");
+        }
+    }
 }
