836bd7643830d5b1361958dc35f1fc05e4032f39
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 14:22:23 +0300

    Create SteamOsImage

--- creational/src/prototype/MacOSCreativeImage.java
+++ creational/src/prototype/MacOSCreativeImage.java
@@ -1,4 +1,8 @@
 package prototype;
-
-public class MacOSCreativeImage {
+public class MacOSCreativeImage implements SystemImage {
+    public SystemImage clone() {
+        try { return (SystemImage) super.clone(); }
+        catch (CloneNotSupportedException e) { return null; }
+    }
+    public String getDetails() { return "macOS Sonoma (Pre-installed: Final Cut, Logic Pro)"; }
 }
