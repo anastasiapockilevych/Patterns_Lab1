2c48312183067abc3b73d634a3e68a202768da2a
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 14:20:41 +0300

    Create MacOSCreativeImage

--- creational/src/prototype/UbuntuServerImage.java
+++ creational/src/prototype/UbuntuServerImage.java
@@ -1,5 +1,3 @@
-
-// --- File: prototype/UbuntuServerImage.java ---
 package prototype;
 public class UbuntuServerImage implements SystemImage {
     public SystemImage clone() {
@@ -7,34 +5,4 @@ public class UbuntuServerImage implements SystemImage {
         catch (CloneNotSupportedException e) { return null; }
     }
     public String getDetails() { return "Ubuntu Server 24.04 (Pre-installed: Docker, Nginx)"; }
-}
-
-// --- File: prototype/MacOSCreativeImage.java ---
-package prototype;
-public class MacOSCreativeImage implements SystemImage {
-    public SystemImage clone() {
-        try { return (SystemImage) super.clone(); }
-        catch (CloneNotSupportedException e) { return null; }
-    }
-    public String getDetails() { return "macOS Sonoma (Pre-installed: Final Cut, Logic Pro)"; }
-}
-
-// --- File: prototype/SteamOsImage.java ---
-package prototype;
-public class SteamOsImage implements SystemImage {
-    public SystemImage clone() {
-        try { return (SystemImage) super.clone(); }
-        catch (CloneNotSupportedException e) { return null; }
-    }
-    public String getDetails() { return "SteamOS 3.0 (Console Mode Optimized)"; }
-}
-
-// --- File: prototype/ChromeOsImage.java ---
-package prototype;
-public class ChromeOsImage implements SystemImage {
-    public SystemImage clone() {
-        try { return (SystemImage) super.clone(); }
-        catch (CloneNotSupportedException e) { return null; }
-    }
-    public String getDetails() { return "ChromeOS Flex (Cloud & Web Apps)"; }
 }
