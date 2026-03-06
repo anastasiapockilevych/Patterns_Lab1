a51ff251b5db125d9ca9d28829315bf7c2184ada
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 13:30:11 +0300

    Create StorageDrive

--- creational/src/builder/ComputerBuilder.java
+++ creational/src/builder/ComputerBuilder.java
@@ -1,4 +1,16 @@
-package builder;
 
-public class ComputerBuilder {
+package builder;
+import factory.StorageDrive;
+import prototype.SystemImage;
+import abstract_factory.PlatformFactory;
+import factory_method.ComputerCase;
+
+public interface ComputerBuilder {
+    void reset();
+    void setSerialNumber(String serial);
+    void buildPlatform(PlatformFactory factory);
+    void installCase(ComputerCase pcCase);
+    void installStorage(StorageDrive storage);
+    void installOs(SystemImage image);
+    Computer getResult();
 }
