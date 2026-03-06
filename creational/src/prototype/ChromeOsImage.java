ba84a8cc8fc9fe6d4650f94791bffedf47717254
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 14:25:07 +0300

    Create SystemConfig

--- creational/src/prototype/ChromeOsImage.java
+++ creational/src/prototype/ChromeOsImage.java
@@ -1,4 +1,8 @@
 package prototype;
-
-public class ChromeOSImage {
+public class ChromeOsImage implements SystemImage {
+    public SystemImage clone() {
+        try { return (SystemImage) super.clone(); }
+        catch (CloneNotSupportedException e) { return null; }
+    }
+    public String getDetails() { return "ChromeOS Flex (Cloud & Web Apps)"; }
 }
