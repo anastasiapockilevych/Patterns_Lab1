84d1501f374cd7a7a7c41981c5f64e413cf0e98d
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 14:27:04 +0300

    Create SerialNumberGenerator

--- creational/src/singleton/InventoryManager.java
+++ creational/src/singleton/InventoryManager.java
@@ -1,4 +1,12 @@
 package singleton;
-
 public class InventoryManager {
+    private static InventoryManager instance;
+    private int totalPartsUsed = 0;
+    private InventoryManager() {}
+    public static synchronized InventoryManager getInstance() {
+        if (instance == null) instance = new InventoryManager();
+        return instance;
+    }
+    public void consumeParts(int count) { totalPartsUsed += count; }
+    public int getUsedParts() { return totalPartsUsed; }
 }
