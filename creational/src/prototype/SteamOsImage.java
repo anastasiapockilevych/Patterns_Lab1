481744a0a211bbe492a53aa65df9bc8cb66ca74b
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 14:23:15 +0300

    Create ChromeOsImage

--- creational/src/prototype/SteamOsImage.java
+++ creational/src/prototype/SteamOsImage.java
@@ -1,4 +1,8 @@
 package prototype;
-
-public class SteamOSImage {
+public class SteamOsImage implements SystemImage {
+    public SystemImage clone() {
+        try { return (SystemImage) super.clone(); }
+        catch (CloneNotSupportedException e) { return null; }
+    }
+    public String getDetails() { return "SteamOS 3.0 (Console Mode Optimized)"; }
 }
