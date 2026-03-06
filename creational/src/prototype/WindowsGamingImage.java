39e9dadf33ee21fc6b603802ad652ce27d7046c7
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 13:50:18 +0300

    Create UbuntuServerImage

--- creational/src/prototype/WindowsGamingImage.java
+++ creational/src/prototype/WindowsGamingImage.java
@@ -1,4 +1,8 @@
 package prototype;
-
-public class WIndowsGamingImage {
+public class WindowsGamingImage implements SystemImage {
+    public SystemImage clone() {
+        try { return (SystemImage) super.clone(); }
+        catch (CloneNotSupportedException e) { return null; }
+    }
+    public String getDetails() { return "Windows 11 Gaming Edition (Pre-installed: Steam, Discord)"; }
 }
