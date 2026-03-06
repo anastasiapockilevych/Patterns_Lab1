3aa4a12d2094583a0d9202d6e9023c580b13aae8
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 13:21:04 +0300

    Create AmdCpu

--- creational/src/abstract_factory/IntelMotherboard.java
+++ creational/src/abstract_factory/IntelMotherboard.java
@@ -1,4 +1,3 @@
 package abstract_factory;
-
-public class IntelMotherboard {
-}
+public class IntelMotherboard implements Motherboard {
+    public String getBoardName() { return "ASUS ROG Z790 (LGA 1700)"; }}
