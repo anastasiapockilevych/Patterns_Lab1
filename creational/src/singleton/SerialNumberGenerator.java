5cb704ff85be8605e8d2500b79b871dd9e136c7c
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 14:27:54 +0300

    Create QuialityControl

--- creational/src/singleton/SerialNumberGenerator.java
+++ creational/src/singleton/SerialNumberGenerator.java
@@ -1,4 +1,11 @@
 package singleton;
-
+import java.util.UUID;
 public class SerialNumberGenerator {
+    private static SerialNumberGenerator instance;
+    private SerialNumberGenerator() {}
+    public static synchronized SerialNumberGenerator getInstance() {
+        if (instance == null) instance = new SerialNumberGenerator();
+        return instance;
+    }
+    public String generate() { return "PC-" + UUID.randomUUID().toString().substring(0, 6).toUpperCase(); }
 }
