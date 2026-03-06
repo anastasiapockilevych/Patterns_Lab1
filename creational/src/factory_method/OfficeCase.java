971d168411e19d807a4c62c4ea7b0f40e7036f2a
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 13:43:09 +0300

    Create ServerCase

--- creational/src/factory_method/OfficeCase.java
+++ creational/src/factory_method/OfficeCase.java
@@ -1,4 +1,4 @@
 package factory_method;
-
-public class OfficeCase {
+public class OfficeCase implements ComputerCase {
+    public String getCaseType() { return "Compact Office Mini-ITX (Silent)"; }
 }
