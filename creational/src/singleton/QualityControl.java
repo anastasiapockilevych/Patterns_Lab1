299f27817df2d055b84cbc9bcaabe4b82e9c5c43
pokhilevych.anastasiia <pokhilevych.anastasiia@chnu.edu.ua>
2026-03-06 14:28:36 +0300

    Create Assembly Logger

--- creational/src/singleton/QualityControl.java
+++ creational/src/singleton/QualityControl.java
@@ -1,4 +1,10 @@
 package singleton;
-
 public class QualityControl {
+    private static QualityControl instance;
+    private QualityControl() {}
+    public static synchronized QualityControl getInstance() {
+        if (instance == null) instance = new QualityControl();
+        return instance;
+    }
+    public boolean passInspection() { return true; } // Завжди успішно для демо
 }
